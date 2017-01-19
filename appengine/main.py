4308987ff9a946f88bc46700edbbc62f9dc538f4
Kevin Kwok <antimatter15@gmail.com>
2017-01-19 01:40:57 -0500

    cors

--- appengine/main.py
+++ appengine/main.py
@@ -15,52 +15,7 @@ import time
 from google.appengine.api import mail
 from google.appengine.ext import ndb
 
-
-# encoded = jwt.encode({'some': 'payload'}, 'secret', algorithm='HS256')
-# 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzb21lIjoicGF5bG9hZCJ9.4twFt5NiznN84AWoo1d7KO1T_yoc0Z6XOpOVswacPZg'
-
-# jwt.decode(encoded, 'secret', algorithms=['HS256'])
-# {'some': 'payload'}
-
-# https://pyjwt.readthedocs.io/en/latest/
-
-
-
 salt = "I am Kanye, king of kings look on my works yeezy mighty and dispair"
-
-
-class HomeHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-
-        self.response.write('hi wayne')
-
-from datetime import timedelta, datetime
-
-
-import string
-import random
-
-class PasswordResetHandler(webapp2.RequestHandler):
-    def get(self):
-        email = self.request.get('email')
-        user = Account.lookup(email)
-        if user is None:
-            self.response.write('no account exists with this email')
-            return
-        
-        user.reset = randstr()
-        user.reset_expire = datetime.now() + timedelta(hours=24)
-        user.put()
-
-        mail.send_mail(sender="noreply@robosheets.appspotmail.com",
-                   to=email,
-                   subject="Reset Robosheets Rassword",
-                   body="""
-Here's your password reset token or something {reset}
-
-""".format(reset=user.reset))
-        self.response.write('chek yo inbox')
 
 
 class Account(ndb.Model):
@@ -68,7 +23,7 @@ class Account(ndb.Model):
     customerId = ndb.StringProperty(indexed=False)
     password = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
-    plan = ndb.StringProperty(indexed=False)
+    subId = ndb.StringProperty(indexed=False)
     
     reset_token = ndb.StringProperty(indexed=True)
     reset_expire = ndb.DateTimeProperty(auto_now_add=False)
@@ -89,7 +44,25 @@ class Account(ndb.Model):
             return None
         email = payload['email']
         return cls.lookup(email)
-        
+
+class Endpoint(webapp2.RequestHandler):
+    def dispatch(self):
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        super(Endpoint, self).dispatch()
+
+class HomeHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+
+        self.response.write('hi wayne')
+
+from datetime import timedelta, datetime
+
+
+import string
+import random
+
+
 
 def randstr():
     return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(14))
@@ -100,17 +73,21 @@ def randstr():
 #        email
 #        token
 #        plan
-class SignupHandler(webapp2.RequestHandler):
+class SignupHandler(Endpoint):
     def post(self):
         email = self.request.get('email')
         token = self.request.get('token')
         plan = self.request.get('plan')
 
         customer = stripe.Customer.create(email=email, source=token)
+        subscription = stripe.Subscription.create(
+            customer=customer.id,
+            plan=plan,
+        )
         user = Account(
             email = email, 
             customerId = customer.id, 
-            plan = plan,
+            subId = subscription.id,
             reset_token = randstr(),
             reset_expire = datetime.now() + timedelta(days=365)
         )
@@ -124,6 +101,7 @@ You signed up for {plan}.
 
 Continue your setup thingy here
 
+This is just some sample form stuff for debugging.
 
 https://robosheets.appspot.com/auth/setup?token={reset}
 
@@ -132,6 +110,20 @@ https://robosheets.appspot.com/auth/setup?token={reset}
 
         self.response.write('chek yo mail dawg')
 
+    def get(self):
+        self.response.write("""
+            <form method="POST">
+                <label>Email: <input name="email" type="email"></label>
+                <label>Stripe Token: <input name="token" type="text"></label>
+                <label>Plan: <select name="plan">
+                    <option value="eliza">eliza</option>
+                    <option value="tamagotchi">tamagotchi</option>
+                    <option value="cleverbot">cleverbot</option>
+                </select></label>
+                <input type="submit">
+            </form>
+        """)
+
 
 
 # - POST /setup
@@ -139,7 +131,7 @@ https://robosheets.appspot.com/auth/setup?token={reset}
 #    params
 #        reset
 #        password
-class PasswordSetupHandler(webapp2.RequestHandler):
+class PasswordSetupHandler(Endpoint):
     def post(self):
 
         reset = self.request.get('reset')
@@ -182,7 +174,7 @@ class PasswordSetupHandler(webapp2.RequestHandler):
 #    params
 #        email
 #        pass
-class LoginHandler(webapp2.RequestHandler):
+class LoginHandler(Endpoint):
     def post(self):
         email = self.request.get('email')
         user = Account.lookup(email)
@@ -221,14 +213,15 @@ class LoginHandler(webapp2.RequestHandler):
 #    responds with a permission level
 #    params
 #        jwt
-class SubscriptionHandler(webapp2.RequestHandler):
+class SubscriptionHandler(Endpoint):
     def get(self):
         user = Account.jwt(self.request.get('jwt'))
         if user is None:
             self.response.write('user not found')
             return
 
-        self.response.write(user.plan)
+        subscription = stripe.Subscription.retrieve(user.subId)
+        self.response.write(subscription.plan.id)
             
 
     def post(self):
@@ -238,12 +231,34 @@ class SubscriptionHandler(webapp2.RequestHandler):
             self.response.write('user not found')
             return
 
-        user.plan = plan
-        user.put()
+        subscription = stripe.Subscription.retrieve(user.subId)
+        subscription.plan = plan
+        subscription.save()
         
         self.response.write('updated subscription')
 
 
+
+class PasswordResetHandler(Endpoint):
+    def get(self):
+        email = self.request.get('email')
+        user = Account.lookup(email)
+        if user is None:
+            self.response.write('no account exists with this email')
+            return
+        
+        user.reset = randstr()
+        user.reset_expire = datetime.now() + timedelta(hours=24)
+        user.put()
+
+        mail.send_mail(sender="noreply@robosheets.appspotmail.com",
+                   to=email,
+                   subject="Reset Robosheets Rassword",
+                   body="""
+Here's your password reset token or something {reset}
+
+""".format(reset=user.reset))
+        self.response.write('chek yo inbox')
 
 
 app = webapp2.WSGIApplication([
